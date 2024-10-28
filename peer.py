98d5314d9f70902494d60f51b181973871b8cd0a
Dian <104989851+DianCrafts@users.noreply.github.com>
2024-10-28 11:17:38 -0400

    Update peer.py

--- peer.py
+++ peer.py
@@ -9,7 +9,6 @@ sock2 = makeserversocket2()
 
 
 def sendData():
-	MESSAGE = "Hello, World!"
 	send_PORT = int(input("enter send port number"))
 	sock2.sendto(str.encode(MESSAGE), (UDP_IP ,send_PORT))
 
